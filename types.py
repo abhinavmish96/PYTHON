34d402df91d066857c6f456ab3de7b9951202b63
Abhinav Mishra <abhinavmish96@gmail.com>
2020-01-08 18:19:24 +0530

    <types>

---
+++ types.py
@@ -0,0 +1,20 @@
+# integer type
+
+myint = 7
+print(myint)
+
+# float type
+
+myfloat = 3.14
+print(myfloat)
+
+# string type
+
+mystr = 'Who are you?'
+print(mystr)
+
+# boolean type
+
+mybool = 1
+print(bool(mybool))
+
