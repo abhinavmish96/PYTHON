c8be54df02b99b72831dd339ccac02841cc692bd
Abhinav Mishra <abhinavmish96@gmail.com>
2020-01-09 21:48:21 +0530

    <variables>

---
+++ variables.py
@@ -0,0 +1,26 @@
+
+# Simple operators can be executed on numbers and strings
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+h = "hello"
+w = "world"
+hw = h + " " + w
+print(hw)
+
+
+#Assignments can be done on more than one variable "simultaneously" on the same line like this
+
+a,b = 1,2
+print(a,b)
+
+
+#Mixing operators between numbers and strings is not supported
+
+one = 1
+two = 2
+h = "hello"
+print(one + two + h)
