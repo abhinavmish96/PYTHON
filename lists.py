a3d2c049f43c2d3c177e51f3491d18f0e9b6a21f
Abhinav Mishra <abhinavmish96@gmail.com>
2020-01-10 19:27:14 +0530

    <lists>

---
+++ lists.py
@@ -0,0 +1,10 @@
+myList = []
+myList.append(1)
+myList.append(2)
+myList.append(3)
+print(myList[0]) # prints 1
+print(myList[1]) # prints 2
+print(myList[2]) # prints 3
+
+for x in myList:
+    print(myList[x])
