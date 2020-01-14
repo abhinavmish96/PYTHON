56ed8b33debbf66a7f73b1b03b686733b218f033
Abhinav Mishra <abhinavmish96@gmail.com>
2020-01-14 20:23:49 +0530

    <operators>

--- operators.py
+++ operators.py
@@ -2,3 +2,9 @@
 
 number = 1 + 2 * 3/10
 print(number)
+
+
+# Another operator available is the modulo (%) operator, which returns the integer remainder of the division. dividend % divisor = remainder
+
+reminder = 11%3
+print(reminder)
