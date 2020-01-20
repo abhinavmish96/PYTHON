9583f06cdb5f16d5a495801f7cfea821baf72413
Abhinav Mishra <abhinavmish96@gmail.com>
2020-01-20 19:38:53 +0530

    <operators>

--- operators.py
+++ operators.py
@@ -27,3 +27,12 @@ print(hw)
 
 lotsOfHellos = "hello" * 10
 print(lotsOfHellos)
+
+# Using Operators with Lists
+
+# Lists can be joined with the addition operators
+
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers =  odd_numbers + even_numbers
+print(all_numbers)
