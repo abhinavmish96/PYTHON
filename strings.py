70dca1894456f2e0b6528bb32379edb4a6ce169b
Abhinav Mishra <abhinavmish96@gmail.com>
2020-01-26 23:42:35 +0530

    <strings>

--- strings.py
+++ strings.py
@@ -11,3 +11,9 @@ print("Hello, %s!" %name)
 name = "Abhi"
 age = 23
 print("%s is %d years old." %(name,age))
+
+# Any object which is not a string can be formatted using the %s operator as well. The string which returns from the "repr" method of that object is formatted as the string
+
+# This prints out: A list: [1, 2, 3]
+mylist = [1,2,3]
+print("A list: %s" % mylist)
