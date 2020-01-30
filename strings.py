ad14d8e6c7aecbfe8820d194f09c7a07225189f4
Abhinav Mishra <abhinavmish96@gmail.com>
2020-01-30 17:58:02 +0530

    <strings>

--- strings.py
+++ strings.py
@@ -29,3 +29,12 @@ astring = "Hello world!"
 print("single quotes are ' '")
 
 print(len(astring))
+
+# That prints out 12, because "Hello world!" is 12 characters long, including punctuation and spaces.
+
+# That prints out 4, because the location of the first occurrence of the letter "o" is 4 characters away from the first character. Notice how there are actually two o's in the phrase - this method only recognizes the first.
+
+# But why didn't it print out 5? Isn't "o" the fifth character in the string? To make things more simple, Python (and most other programming languages) start things at 0 instead of 1. So the index of "o" is 4.
+
+astring = "Hello World!"
+print(astring.index("o"))
