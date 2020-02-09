ab0d1c6c0c89da00123376949a5be5ff6482b67a
Abhinav Mishra <abhinavmish96@gmail.com>
2020-02-09 17:57:46 +0530

    <conditions>

--- conditions.py
+++ conditions.py
@@ -6,3 +6,13 @@ x = 2
 print( x == 2)
 print( x == 3)
 print(x < 3)
+
+# Boolean operators; The "and" and "or" boolean operators allow building complex boolean expressions, for example
+
+name = "John"
+age = 23
+if name == "John" and age == 23:
+    print("Your name is John, and you are also 23 years old.")
+
+if name == "John" or name == "Rick":
+    print("Your name is either John or Rick.")
