4f2e4e6bd04b792ba18e578d0ef716e38b5a86b3
Abhinav Mishra <abhinavmish96@gmail.com>
2020-02-17 18:44:40 +0530

    <loops>

---
+++ loops.py
@@ -0,0 +1,9 @@
+# There are two types of loops in Python, for and while.
+
+# The "for" loop
+
+# For loops iterate over a given sequence. Here is an example:
+
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
