e36811e5cf4b7b153e98be9d1071846b168e79c4
Abhinav Mishra <abhinavmish96@gmail.com>
2020-02-24 20:36:13 +0530

    <functions>

---
+++ function.py
@@ -0,0 +1,3 @@
+# What are Functions?
+
+# Functions are a convenient way to divide your code into useful blocks, allowing us to order our code, make it more readable, reuse it and save some time. Also functions are a key way to define interfaces so programmers can share their code.
