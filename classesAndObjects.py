e858c9450b4399e7a29ceec58f1698089caef50c
Abhinav Mishra <abhinavmish96@gmail.com>
2020-03-02 19:47:34 +0530

    <functions>

---
+++ classesAndObjects.py
@@ -0,0 +1,9 @@
+# Objects are an encapsulation of variables and functions into a single entity. Objects get their variables and functions from classes. Classes are essentially a template to create your objects.
+
+# A very basic class would look something like this:
+
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
