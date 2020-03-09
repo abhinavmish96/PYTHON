7e109a6fe2c7fa7fa6846f28dd8cf565d90c36f7
Abhinav Mishra <abhinavmish96@gmail.com>
2020-03-09 14:40:10 +0530

    <functions>

--- classesAndObjects.py
+++ classesAndObjects.py
@@ -32,3 +32,15 @@ class MyClass:
 myobjectx = MyClass()
 
 myobjectx.variable
+
+# So for instance the below would output the string "blah":
+
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+print(myobjectx.variable)
