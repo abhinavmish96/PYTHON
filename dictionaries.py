d9f7669c6ed24551c5aba48ee4cc957344f3c669
Abhinav Mishra <abhinavmish96@gmail.com>
2020-03-21 13:57:32 +0530

    <dictionaries>

--- dictionaries.py
+++ dictionaries.py
@@ -24,3 +24,15 @@ print(phonebook)
 phonebook = {"John" : 938477566,"Jack" : 938377264,"Jill" : 947662781}
 for name, number in phonebook.items():
     print("Phone number of %s is %d" % (name, number))
+
+# Removing a value
+# To remove a specified index, use either one of the following notations:
+
+phonebook = {
+   "John" : 938477566,
+   "Jack" : 938377264,
+   "Jill" : 947662781
+}
+del phonebook["John"]
+print(phonebook)
+
